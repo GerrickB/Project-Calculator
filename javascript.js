8bc2f5fba4cb83bf833e006cd748295bbb7b69af
Gerrick B <lsgenether@gmail.com>
2022-07-21 19:00:04 -0700

    Edit math functions and clear function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,101 @@
+function calculate(numEntered) {
+  if (numEntered == 'C') {
+    document.getElementById('answer').value = '';
+    firstNum = 0;
+    secondNum = 0;
+    operatorBool = '';
+    operatorPass = '';
+  }
+  else if (numEntered == '+') {
+    firstNum = parseFloat(document.getElementById('answer').value);
+    if (operatorPass == '+') {
+      secondNum = parseFloat(document.getElementById('answer').value);
+      firstNum = firstNum + secondNum;
+      document.getElementById('answer').value = firstNum;
+      operatorPass = '';
+    }
+    operatorPass = numEntered;
+    operatorBool = numEntered;
+  }
+  else if (numEntered == '-') {
+    firstNum = parseFloat(document.getElementById('answer').value);
+    operatorPass = numEntered;
+    operatorBool = numEntered;
+  }
+  else if (numEntered == '*') {
+    firstNum = parseFloat(document.getElementById('answer').value);
+    operatorPass = numEntered;
+    operatorBool = numEntered;
+  }
+  else if (numEntered == '/') {
+    firstNum = parseFloat(document.getElementById('answer').value);
+    operatorPass = numEntered;
+    operatorBool = numEntered;
+  }
+  else if (numEntered == '=') {
+    secondNum = parseFloat(document.getElementById('answer').value);
+    operate(firstNum, secondNum, operatorPass);
+  }
+  else {
+    if (operatorBool != '') {
+      operatorBool = '';
+      document.getElementById('answer').value = '';
+    }
+    document.getElementById('answer').value += numEntered;
+  }
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+  if (operator == '+') {
+    document.getElementById('answer').value = add(num1, num2);
+  }
+  if (operator == '-') {
+    document.getElementById('answer').value = subtract(num1, num2);
+  }
+  if (operator == '*') {
+    document.getElementById('answer').value = multiply(num1, num2);
+  }
+  if (operator == '/') {
+    document.getElementById('answer').value = divide(num1, num2);
+  }
+}
+
+let firstNum = 0;
+let secondNum = 0;
+let operatorBool = '';
+let operatorPass = '';
+
+/*function add(...theArgs) {
+  const numbers = [];
+  let result = 0;
+  numbers.push(...theArgs);
+  console.log(numbers);
+
+  for (let i = 0; i < numbers.length; i++) {
+      result += numbers[i];
+  }
+
+  console.log(result);
+  //document.getElementById('answer').value = result;
+  return result;
+}*/
+
 /*function calculate(numEntered) {
     if (numEntered == 'C') {
       document.getElementById('answer').value = '';
@@ -10,49 +108,3 @@
     }
   } 
 */
-function calculate(numEntered) {
-  if (numEntered == 'C') {
-    document.getElementById('answer').value = '';
-  }
-  else if (numEntered == '+') {
-    number = parseFloat(document.getElementById('answer').value);
-  }
-  else {
-  document.getElementById('answer').value += numEntered;
-  }
-}
-
-function add(...theArgs) {
-    const numbers = [];
-    let result = 0;
-    numbers.push(...theArgs);
-    console.log(numbers);
-
-    for (let i = 0; i < numbers.length; i++) {
-        result += numbers[i];
-    }
-
-    console.log(result);
-}
-
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num1/num2;
-}
-
-function operate(num1, num2, operator) {
-  if (operator == '+') {
-    add(num1, num2);
-  }
-}
-
-let firstNum = 0;
-let SecondNum = 0;
-let 
